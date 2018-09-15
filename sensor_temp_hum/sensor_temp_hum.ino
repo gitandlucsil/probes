77d9a7e28eaa454f5242bc66ef9f679594aea08f
andre <andlucsil1221@gmail.com>
2018-09-14 22:15:33 -0300

    Criação das mensagens JSON

--- sensor_temp_hum/sensor_temp_hum.ino
+++ sensor_temp_hum/sensor_temp_hum.ino
@@ -1,77 +1,91 @@
 #include "DHT.h" //Biblioteca retirada de https://github.com/adafruit/DHT-sensor-library
+#include "ArduinoJson.h" //Biblioteca retirada de https://github.com/bblanchon/ArduinoJson
 
 #define DHTPIN_1 A1
 #define DHTPIN_2 A2
-#define DHTPIN_3 A4
-#define DHTPIN_4 A5
 #define DHTTYPE DHT11
 
 DHT dht_1(DHTPIN_1, DHTTYPE);
 DHT dht_2(DHTPIN_2, DHTTYPE);
-DHT dht_3(DHTPIN_3, DHTTYPE);
-/*DHT dht_4(DHTPIN_4, DHTTYPE);*/
 
-void setup() {
+struct probe_t_h{
+  int humidity;
+  int temperature;
+};
+
+probe_t_h probe1;
+probe_t_h probe2;
+
+void getUmidity(int *h_1, int *h_2)
+{
+  *h_1 = (int) dht_1.readHumidity();
+  *h_2 = (int) dht_2.readHumidity();
+}
+
+void getTemperature(int *t_1, int *t_2)
+{
+  *t_1 = (int) dht_1.readTemperature();
+  *t_2 = (int) dht_2.readTemperature();
+}
+
+DynamicJsonBuffer jsonBuffer;
+
+void setup()
+{
   Serial.begin(9600);
   dht_1.begin();
   dht_2.begin();
-  dht_3.begin();
-  /*dht_4.begin();*/
 }
 
 void loop() {
-  float h_1 = dht_1.readHumidity();
-  float t_1 = dht_1.readTemperature();
-  float h_2 = dht_2.readHumidity();
-  float t_2 = dht_2.readTemperature();
-  float h_3 = dht_3.readHumidity();
-  float t_3 = dht_3.readTemperature();
-  /*float h_4 = dht_4.readHumidity();
-  float t_4 = dht_4.readTemperature();*/
   
-  delay(2000);
-  if(isnan(t_1) || isnan(h_1)){
+  getUmidity(&probe1.humidity,&probe2.humidity);
+  getTemperature(&probe1.temperature,&probe2.temperature);
+  JsonObject& json = jsonBuffer.createObject();
+  json["temp_1"] = probe1.temperature;
+  json["temp_2"] = probe2.temperature;
+  json["hum_1"] = probe1.humidity;
+  json["hum_2"] = probe2.humidity;
+  json.printTo(Serial);
+  delay(5000);
+  /*int humidity_1 = (int) dht_1.readHumidity();
+  int temperature_1 = (int) dht_1.readTemperature();
+  int humidity_2 = (int) dht_2.readHumidity();
+  int temperature_2 = (int) dht_2.readTemperature();
+  int humidity_3 = (int) dht_3.readHumidity();
+  int temperature_3 = (int) dht_3.readTemperature();*/
+ 
+  /*if(isnan(probe1.temperature) || isnan(probe1.humidity)){
     Serial.println("Falha ao ler do sensor DHT 1!");
   }else{
     Serial.print("Umidade Sensor 1: ");
-    Serial.print(h_1);
+    Serial.print(probe1.humidity);
     Serial.print(" %");
     Serial.print("Temperatura Sensor 1: ");
-    Serial.print(t_1);
+    Serial.print(probe1.temperature);
     Serial.print(" ºC");
     Serial.print("\n");
   }
-  if(isnan(t_2) || isnan(h_2)){
+  if(isnan(probe2.temperature) || isnan(probe2.humidity)){
     Serial.println("Falha ao ler do sensor DHT 2!");
   }else{
     Serial.print("Umidade Sensor 2: ");
-    Serial.print(h_2);
+    Serial.print(probe2.humidity);
     Serial.print(" %");
     Serial.print("Temperatura Sensor 2: ");
-    Serial.print(t_2);
+    Serial.print(probe2.temperature);
     Serial.print(" ºC");
     Serial.print("\n");
   }
-  if(isnan(t_3) || isnan(h_3)){
+  if(isnan(probe3.temperature) || isnan(probe3.humidity)){
     Serial.println("Falha ao ler do sensor DHT 3!");
   }else{
     Serial.print("Umidade Sensor 3: ");
-    Serial.print(h_3);
+    Serial.print(probe3.humidity);
     Serial.print(" %");
     Serial.print("Temperatura Sensor 3: ");
-    Serial.print(t_3);
+    Serial.print(probe3.temperature);
     Serial.print(" ºC");
-    Serial.print("\n");
-  }
-  /*if(isnan(t_4) || isnan(h_4)){
-    Serial.println("Falha ao ler do sensor DHT 4!");
-  }else{
-    Serial.print("Umidade Sensor 4: ");
-    Serial.print(h_4);
-    Serial.print(" %");
-    Serial.print("Temperatura Sensor 4: ");
-    Serial.print(t_4);
-    Serial.print(" ºC");
-    Serial.print("\n");
-  }*/
+    Serial.print("\n");*/
+
 }
